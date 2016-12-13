d2d37272def801f5602789604f639b0e3a1c067d
Hugo Holmqvist <hugo.holm@live.com>
2016-12-13 14:24:05 +0200

    Change to onText instead of on message listeners

--- app.js
+++ app.js
@@ -7,34 +7,35 @@ const config = JSON.parse(FS.readFileSync('conf', 'utf8'));
 
 var client = new TelegramClient(config.alkoBot, { polling: true });
 
-client.on('message', (msg) => {
-  if (msg.text === '/jalluindeksi') {
-    needle.get('http://droptable.tk:8080/products/000706', (err, res) => {
-      client.sendMessage(msg.chat.id, 'Jallun hinta: ' + res.body.data.price + '€');
-    });
-  } else if (msg.text.startsWith('/price')) {
-    let arg = msg.text.split(' ')[1];
-    if (arg) {
-      needle.get('http://droptable.tk:8080/products', (err, res) => {
-        let products = [];
-        for (let i in res.body.data) {
-          let o = res.body.data[i];
-          try {
-            if (o.name && o.name.toLowerCase().indexOf(arg.toLowerCase()) > -1) {
-              products.push(o);
-            }
-          } catch (e) {
-            console.log(e);
+client.onText(/\/jalluindeksi/, msg => {
+  needle.get('http://droptable.tk:8080/products/000706', (err, res) => {
+    client.sendMessage(msg.chat.id, 'Jallun hinta: ' + res.body.data.price + '€');
+  });
+});
+
+client.onText(/\/price/, msg => {
+  let arg = msg.text.split(' ')[1];
+  if (arg) {
+    needle.get('http://droptable.tk:8080/products', (err, res) => {
+      let products = [];
+      for (let i in res.body.data) {
+        let o = res.body.data[i];
+        try {
+          if (o.name && o.name.toLowerCase().indexOf(arg.toLowerCase()) > -1) {
+            products.push(o);
           }
+        } catch (e) {
+          console.log(e);
         }
+      }
 
-        let resStr = '';
-        for (let o of products) {
-          resStr += `${o.name.replace('*', '1t').replace('***', '3t')} | ${o.volume}l  | *${o.price}€*\n`;
-        }
+      let resStr = '';
+      for (let o of products) {
+        resStr += `${o.name.replace('*', '1t').replace('***', '3t')} | ${o.volume}l  | *${o.price}€*\n`;
+      }
 
-        client.sendMessage(msg.chat.id, resStr, { parse_mode: 'markdown' });
-      });
-    }
+      client.sendMessage(msg.chat.id, resStr, { parse_mode: 'markdown' });
+    });
   }
 });
+
